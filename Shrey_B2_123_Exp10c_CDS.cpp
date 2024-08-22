9b6b6fccf8ce3691b62843ce86e03fac139d00b9
Shrey raj <shreyraj37@gmail.com>
2024-08-23 01:36:24 +0530

    Add files via upload

---
+++ Shrey_B2_123_Exp10c_CDS.cpp
@@ -0,0 +1,29 @@
+
+// NAME - SHREY RAJ
+// PRN - 23070123123 
+// EXPERIMENT - 10(C)           
+
+// Swapping the values using call by reference  
+
+#include<iostream> 
+using namespace std; 
+void swap(int *x, int *y) 
+{
+    int swap;
+    swap=*x;
+    *x=*y;
+    *y=swap;
+}
+
+int main() 
+{
+    int a=4,b=7;
+    swap(&a,&b);
+    cout<<"Value of a is: "<<a<<"\n";
+    cout<<"Value of b is: "<<b<<"\n";
+    return 0;
+}
+
+// Value of a is: 7
+// Value of b is: 4            
+
